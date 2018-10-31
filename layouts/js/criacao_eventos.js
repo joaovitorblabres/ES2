f0fd835bf0dd8c47788befba375d3f54ba89a8ab
Joao Vitor Labres <aluno-uffs@lab-comp.cco.uffs.edu.br>
2018-10-31 10:49:10 -0300

    cadastro de eventos iniciado

--- layouts/js/criacao_eventos.js
+++ layouts/js/criacao_eventos.js
@@ -5,17 +5,15 @@ function initMap() {
     lng: -52.615699
   };
 
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById('mapouter'), {
     zoom: 14,
     center: myLatLng
   });
    //ainda n está pronto
-// Using the function to create multiple markers
-  geocodeLatLng("52.077937, 4.311377","Apple Store","Apple Inc. is an American multinational technology company headquartered in Cupertino, California, that designs, develops, and sells consumer electronics, computer software, and online services.");
+   // Using the function to create multiple markers
   geocodeLatLng("-27.1009343, -52.615699","TESTE","SLA");
-  geocodeLatLng("52.079572, 4.313265","Ridderzaal","The Ridderzaal is the main building of the 13th century Binnenhof in The Hague, Netherlands. It is used for the state opening of Parliament on Prinsjesdag, when the Dutch monarch drives to Parliament in the Golden Coach and delivers the speech from the throne. It is also used for official royal receptions, and interparliamentary conferences.")
-
-var infowindow = new google.maps.InfoWindow({maxWidth: 300});
+  var markers = [];
+  var infowindow = new google.maps.InfoWindow({maxWidth: 300});
 
   function geocodeLatLng(location,title,body) {
     var geocoder = new google.maps.Geocoder;
@@ -29,6 +27,7 @@ var infowindow = new google.maps.InfoWindow({maxWidth: 300});
             position: latlng,
             map: map
           });
+          markers.push(marker);
 
           marker.addListener('click', function() {
             infowindow.setContent("<h3>" + title + "</h3>" + "<p>" + body + "</p>");
@@ -42,6 +41,30 @@ var infowindow = new google.maps.InfoWindow({maxWidth: 300});
         window.alert('Geocoder failed due to: ' + status);
       }
     });
+  }
+  google.maps.event.addListener(map, 'click', function(event) {
+    var lat = event.latLng.lat();
+    var lng = event.latLng.lng();
+    document.getElementById('lati').value = lat;
+    document.getElementById('long').value = lng;
+    placeMarker(event.latLng);
+  });
+  google.maps.event.addListener(map, 'rightclick', function(event) {
+    removeLast();
+    document.getElementById('lati').value = 0;
+    document.getElementById('long').value = 0;
+  });
+  function removeLast(location) {
+    markers[markers.length-1].setMap(null);
+    markers.pop();
+  }
+
+  function placeMarker(location) {
+      var marker = new google.maps.Marker({
+          position: location,
+          map: map
+      });
+      markers.push(marker);
   }
 
 }
